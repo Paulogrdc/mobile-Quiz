de6359b62609aa4e7126771af9c328128c3b54ca
Paulo <Paulogrdc@gmail.com>
2025-02-10 20:35:10 -0300

    mexi no javaScript das perguntas

--- Script/javaScript.js
+++ Script/javaScript.js
@@ -12,18 +12,116 @@ function verificarResposta() {
 
     span1.style.backgroundColor = "rgba(255, 0, 0, 0.800)"
     span1.style.borderRadius = "5px"
-    span1.style.width = "100px"
+    span1.style.width = "500px"
     
     span2.style.backgroundColor = "rgba(0, 128, 0, 0.600)"
     span2.style.borderRadius = "5px"
-    span2.style.width = "100px"
+    span2.style.width = "500px"
     
     span3.style.backgroundColor = "rgba(255, 0, 0, 0.800)"
     span3.style.borderRadius = "5px"
-    span3.style.width = "100px"
+    span3.style.width = "500px"
 
     span4.style.backgroundColor = "rgba(255, 0, 0, 0.800)"
     span4.style.borderRadius = "5px"
-    span4.style.width = "100px"
+    span4.style.width = "500px"
+
+}
+
+
+
+ 
+function verificarResposta2() {
+    const span1  = document.getElementById("100%")
+    const span2  = document.getElementById("10%") 
+    const span3 = document.getElementById("40%") 
+    const span4  = document.getElementById("60%")
+
+    const Respostas = document.querySelectorAll(".respostas")
+
+    Respostas.forEach(Res=> {
+        Res.style.fontSize = "15px"  
+    });
+
+    span1.style.backgroundColor = "rgba(255, 0, 0, 0.800)"
+    span1.style.borderRadius = "5px"
+    span1.style.width = "500px"
+    
+    span2.style.backgroundColor = "rgba(255, 0, 0, 0.800)"
+    span2.style.borderRadius = "5px"
+    span2.style.width = "500px"
+    
+    span3.style.backgroundColor = "rgba(255, 0, 0, 0.800)"
+    span3.style.borderRadius = "5px"
+    span3.style.width = "500px"
+
+    span4.style.backgroundColor = "rgba(0, 128, 0, 0.600)"
+    span4.style.borderRadius = "5px"
+    span4.style.width = "500px"
+
+}
+
+
+
+
+function verificarResposta3() {
+    const span1  = document.getElementById("100%")
+    const span2  = document.getElementById("10%") 
+    const span3 = document.getElementById("40%") 
+    const span4  = document.getElementById("60%")
+
+    const Respostas = document.querySelectorAll(".respostas")
+
+    Respostas.forEach(Res=> {
+        Res.style.fontSize = "15px"  
+    });
+
+    span1.style.backgroundColor = "rgba(255, 0, 0, 0.800)"
+    span1.style.borderRadius = "5px"
+    span1.style.width = "500px"
+    
+    span2.style.backgroundColor = "rgba(255, 0, 0, 0.800)"
+    span2.style.borderRadius = "5px"
+    span2.style.width = "500px"
+    
+    span3.style.backgroundColor = "rgba(0, 128, 0, 0.600)"
+    span3.style.borderRadius = "5px"
+    span3.style.width = "500px"
+
+    span4.style.backgroundColor = "rgba(255, 0, 0, 0.800)"
+    span4.style.borderRadius = "5px"
+    span4.style.width = "500px"
+
+}
+ 
+
+
+function verificarResposta4() {
+    const span1  = document.getElementById("100%")
+    const span2  = document.getElementById("10%") 
+    const span3 = document.getElementById("40%") 
+    const span4  = document.getElementById("60%")
+
+    const Respostas = document.querySelectorAll(".respostas")
+
+    Respostas.forEach(Res=> {
+        Res.style.fontSize = "15px"  
+    });
+
+    span1.style.backgroundColor = "rgba(0, 128, 0, 0.600)"
+    span1.style.borderRadius = "5px"
+    span1.style.width = "500px"
+    
+    span2.style.backgroundColor = "rgba(255, 0, 0, 0.800)"
+    span2.style.borderRadius = "5px"
+    span2.style.width = "500px"
+    
+    span3.style.backgroundColor = "rgba(255, 0, 0, 0.800)"
+    span3.style.borderRadius = "5px"
+    span3.style.width = "500px"
+
+    span4.style.backgroundColor = "rgba(255, 0, 0, 0.800)"
+    span4.style.borderRadius = "5px"
+    span4.style.width = "700px"
 
 }
